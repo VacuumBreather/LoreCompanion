93b810b57c8f8b57602a209e49fc68372289bb08
Andreas Schmitt <andreas@vacuumbreather.de>
2026-09-20 00:11:42 +0200

    Add EpisodesViewModel

---
+++ LoreCompanion/ViewModels/EpisodesViewMode.cs
@@ -0,0 +1,11 @@
+﻿namespace LoreCompanion.ViewModels
+{
+    public class EpisodesViewMode : SectionScreen
+    {
+        public EpisodesViewMode()
+            : base(NavigationSection.Overview)
+        {
+            DisplayName = "Episodes";
+        }
+    }
+}
