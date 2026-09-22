e92cc6287d3fe47c08f22e9b8be4dcd83a830ce5
Andreas Schmitt <andreas@vacuumbreather.de>
2026-09-22 03:53:44 +0200

    Introduce admin mode

--- LoreCompanion/ViewModels/ShellViewModel.cs
+++ LoreCompanion/ViewModels/ShellViewModel.cs
@@ -30,6 +30,8 @@ namespace LoreCompanion.ViewModels
             _dialogService = dialogService;
             _notificationService = notificationService;
 
+            IsAdminMode = Environment.GetCommandLineArgs().Contains("--admin");
+
             ItemsView = (ListCollectionView)CollectionViewSource.GetDefaultView(Items);
             ItemsView.GroupDescriptions!.Add(new PropertyGroupDescription(nameof(SectionScreen.Section)));
 
@@ -59,6 +61,8 @@ namespace LoreCompanion.ViewModels
             typeof(ShellViewModel).Assembly.GetName().Version ?? Version.Parse("0.0.0");
 
         public ListCollectionView ItemsView { get; }
+
+        public bool IsAdminMode { get; }
 
         private static ILogger Logger { get; } = LogManager.GetLogger();
 
---
+++ LoreCompanion/Views/Helpers/AdminHelper.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace LoreCompanion.Views.Helpers
+{
+    public static class AdminHelper
+    {
+        public static readonly DependencyProperty IsAdminModeProperty = DependencyProperty.RegisterAttached(
+            DependencyPropertyNameHelper.GetName(nameof(IsAdminModeProperty)),
+            typeof(bool),
+            typeof(AdminHelper),
+            new FrameworkPropertyMetadata(false, FrameworkPropertyMetadataOptions.Inherits));
+
+        public static void SetIsAdminMode(DependencyObject element, bool value)
+        {
+            element.SetValue(IsAdminModeProperty, value);
+        }
+
+        public static bool GetIsAdminMode(DependencyObject element)
+        {
+            return (bool)element.GetValue(IsAdminModeProperty);
+        }
+    }
+}
