803b19c6d9bef5913aa3aa92c30c3d4a5df00f6f
Andreas Schmitt <andreas@vacuumbreather.de>
2026-09-23 01:30:26 +0200

    Hide delete item button in admin mode

--- LoreCompanion/Views/Helpers/AdminHelper.cs
+++ LoreCompanion/Views/Helpers/AdminHelper.cs
@@ -6,6 +6,6 @@ namespace LoreCompanion.Views.Helpers
     public static class AdminHelper
     {
         public static readonly Visibility AdminModeVisibility =
-            AppHelper.IsAdminMode ? Visibility.Visible : Visibility.Collapsed;
+            AppHelper.IsAdminMode ? Visibility.Visible : Visibility.Hidden;
     }
 }
