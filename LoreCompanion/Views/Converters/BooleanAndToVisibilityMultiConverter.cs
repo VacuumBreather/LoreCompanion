5a9dc09cd8140ed6c1be833e033ea47c236cf395
Andreas Schmitt <andreas@vacuumbreather.de>
2026-09-23 06:46:37 +0200

    Extract common view elements to MasterDetailControl

---
+++ LoreCompanion/Views/Converters/BooleanAndToVisibilityMultiConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace LoreCompanion.Views.Converters
+{
+    public class BooleanAndToVisibilityMultiConverter : IMultiValueConverter
+    {
+        public Visibility TrueVisibility { get; set; } = Visibility.Visible;
+
+        public Visibility FalseVisibility { get; set; } = Visibility.Collapsed;
+
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return values.All(value => value is true) ? TrueVisibility : FalseVisibility;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
---
+++ LoreCompanion/Views/MasterDetailControl.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace LoreCompanion.Views
+{
+    public partial class MasterDetailControl : UserControl
+    {
+        public static readonly DependencyProperty ItemTemplateProperty = DependencyProperty.Register(
+            nameof(ItemTemplate),
+            typeof(DataTemplate),
+            typeof(MasterDetailControl));
+
+        public static readonly DependencyProperty DetailTemplateProperty = DependencyProperty.Register(
+            nameof(DetailTemplate),
+            typeof(DataTemplate),
+            typeof(MasterDetailControl));
+
+        public MasterDetailControl()
+        {
+            InitializeComponent();
+        }
+
+        public DataTemplate? ItemTemplate
+        {
+            get => (DataTemplate?)GetValue(ItemTemplateProperty);
+            set => SetValue(ItemTemplateProperty, value);
+        }
+
+        public DataTemplate? DetailTemplate
+        {
+            get => (DataTemplate?)GetValue(DetailTemplateProperty);
+            set => SetValue(DetailTemplateProperty, value);
+        }
+    }
+}
