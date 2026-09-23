59347a1c3a57b1feec0db9a682057de533325d73
Andreas Schmitt <andreas@vacuumbreather.de>
2026-09-23 04:54:45 +0200

    Scroll selected item into view after filter change

--- LoreCompanion/Views/Helpers/ListBoxHelper.cs
+++ LoreCompanion/Views/Helpers/ListBoxHelper.cs
@@ -1,5 +1,7 @@
-﻿using System.Windows;
+﻿using System.Collections.Specialized;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Threading;
 
 namespace LoreCompanion.Views.Helpers
 {
@@ -11,6 +13,13 @@ namespace LoreCompanion.Views.Helpers
             typeof(ListBoxHelper),
             new PropertyMetadata(false, OnScrollToSelectedItemChanged));
 
+        private static readonly DependencyProperty ItemsCollectionChangedHandlerProperty =
+            DependencyProperty.RegisterAttached(
+                DependencyPropertyNameHelper.GetName(nameof(ItemsCollectionChangedHandlerProperty)),
+                typeof(NotifyCollectionChangedEventHandler),
+                typeof(ListBoxHelper),
+                new PropertyMetadata(null));
+
         public static void SetScrollToSelectedItem(DependencyObject element, bool value)
         {
             element.SetValue(ScrollToSelectedItemProperty, value);
@@ -21,11 +30,28 @@ namespace LoreCompanion.Views.Helpers
             return (bool)element.GetValue(ScrollToSelectedItemProperty);
         }
 
+        private static void OnListBoxLoaded(object sender, RoutedEventArgs e)
+        {
+            if (sender is ListBox listBox)
+            {
+                SubscribeToItemsCollectionChanged(listBox);
+                ScrollSelectedItemIntoView(listBox);
+            }
+        }
+
+        private static void OnListBoxUnloaded(object sender, RoutedEventArgs e)
+        {
+            if (sender is ListBox listBox)
+            {
+                UnsubscribeFromItemsCollectionChanged(listBox);
+            }
+        }
+
         private static void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            if (sender is ListBox { SelectedItem: not null } listBox)
+            if (sender is ListBox listBox)
             {
-                listBox.ScrollIntoView(listBox.SelectedItem);
+                ScrollSelectedItemIntoView(listBox);
             }
         }
 
@@ -39,11 +65,70 @@ namespace LoreCompanion.Views.Helpers
             if ((bool)e.NewValue)
             {
                 listBox.SelectionChanged += OnSelectionChanged;
+                listBox.Loaded += OnListBoxLoaded;
+                listBox.Unloaded += OnListBoxUnloaded;
+
+                if (listBox.IsLoaded)
+                {
+                    SubscribeToItemsCollectionChanged(listBox);
+                }
             }
             else
             {
                 listBox.SelectionChanged -= OnSelectionChanged;
+                listBox.Loaded -= OnListBoxLoaded;
+                listBox.Unloaded -= OnListBoxUnloaded;
+                UnsubscribeFromItemsCollectionChanged(listBox);
             }
+        }
+
+        private static void SubscribeToItemsCollectionChanged(ListBox listBox)
+        {
+            UnsubscribeFromItemsCollectionChanged(listBox);
+
+            if (listBox.Items is not INotifyCollectionChanged notifyCollection)
+            {
+                return;
+            }
+
+            NotifyCollectionChangedEventHandler handler = (_, _) => ScrollSelectedItemIntoView(listBox);
+            listBox.SetValue(ItemsCollectionChangedHandlerProperty, handler);
+            notifyCollection.CollectionChanged += handler;
+        }
+
+        private static void UnsubscribeFromItemsCollectionChanged(ListBox listBox)
+        {
+            if (listBox.GetValue(ItemsCollectionChangedHandlerProperty) is not NotifyCollectionChangedEventHandler
+                handler)
+            {
+                return;
+            }
+
+            if (listBox.Items is INotifyCollectionChanged notifyCollection)
+            {
+                notifyCollection.CollectionChanged -= handler;
+            }
+
+            listBox.ClearValue(ItemsCollectionChangedHandlerProperty);
+        }
+
+        private static void ScrollSelectedItemIntoView(ListBox listBox)
+        {
+            if (listBox.SelectedItem is null)
+            {
+                return;
+            }
+
+            // Defer execution until after the layout pass / container generation is complete
+            listBox.Dispatcher.BeginInvoke(
+                DispatcherPriority.Loaded,
+                () =>
+                {
+                    if (listBox.SelectedItem is not null)
+                    {
+                        listBox.ScrollIntoView(listBox.SelectedItem);
+                    }
+                });
         }
     }
 }
