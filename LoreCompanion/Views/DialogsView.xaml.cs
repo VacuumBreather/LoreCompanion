40b095b75322fb5db6ccf969ec807e15abd01330
Andreas Schmitt <andreas@vacuumbreather.de>
2026-09-24 19:17:01 +0200

    Add DetailsView with Master template

---
+++ LoreCompanion/Views/DialogsView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace LoreCompanion.Views
+{
+    public partial class DialogsView : UserControl
+    {
+        public DialogsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
