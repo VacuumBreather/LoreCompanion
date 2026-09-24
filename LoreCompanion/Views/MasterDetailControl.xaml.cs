9e19baa304b21a28031dee4d4e69d9162b3e5cd2
Andreas Schmitt <andreas@vacuumbreather.de>
2026-09-25 01:18:48 +0200

    Separate read-only and edit versions of detail templates

--- LoreCompanion/Views/MasterDetailControl.xaml.cs
+++ LoreCompanion/Views/MasterDetailControl.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using LoreCompanion.Utilities;
 using LoreCompanion.ViewModels;
 
 namespace LoreCompanion.Views
@@ -7,11 +8,6 @@ namespace LoreCompanion.Views
     {
         public static readonly DependencyProperty ItemTemplateProperty = DependencyProperty.Register(
             nameof(ItemTemplate),
-            typeof(DataTemplate),
-            typeof(MasterDetailControl));
-
-        public static readonly DependencyProperty DetailTemplateProperty = DependencyProperty.Register(
-            nameof(DetailTemplate),
             typeof(DataTemplate),
             typeof(MasterDetailControl));
 
@@ -25,6 +21,11 @@ namespace LoreCompanion.Views
             typeof(DataTemplate),
             typeof(MasterDetailControl));
 
+        private static readonly DependencyProperty DetailTemplateProperty = DependencyProperty.Register(
+            nameof(DetailTemplate),
+            typeof(DataTemplate),
+            typeof(MasterDetailControl));
+
         public static readonly DependencyProperty EditModeProperty = DependencyProperty.Register(
             nameof(EditMode),
             typeof(EditMode),
@@ -34,18 +35,14 @@ namespace LoreCompanion.Views
         public MasterDetailControl()
         {
             InitializeComponent();
+
+            Loaded += OnLoaded;
         }
 
         public DataTemplate? ItemTemplate
         {
             get => (DataTemplate?)GetValue(ItemTemplateProperty);
             set => SetValue(ItemTemplateProperty, value);
-        }
-
-        public DataTemplate? DetailTemplate
-        {
-            get => (DataTemplate?)GetValue(DetailTemplateProperty);
-            set => SetValue(DetailTemplateProperty, value);
         }
 
         public DataTemplate? ReadOnlyDetailTemplate
@@ -66,6 +63,17 @@ namespace LoreCompanion.Views
             set => SetValue(EditModeProperty, value);
         }
 
+        private DataTemplate? DetailTemplate
+        {
+            get => (DataTemplate?)GetValue(DetailTemplateProperty);
+            set => SetValue(DetailTemplateProperty, value);
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            SetDetailTemplate();
+        }
+
         private static void OnEditModeChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
             if (d is not MasterDetailControl masterDetailControl)
@@ -73,11 +81,19 @@ namespace LoreCompanion.Views
                 return;
             }
 
-            var editMode = (EditMode)e.NewValue;
+            masterDetailControl.SetDetailTemplate();
+        }
 
-            masterDetailControl.DetailTemplate = editMode == EditMode.Edit
-                                                     ? masterDetailControl.EditDetailTemplate
-                                                     : masterDetailControl.ReadOnlyDetailTemplate;
+        private void SetDetailTemplate()
+        {
+            if (!AppHelper.IsAdminMode)
+            {
+                DetailTemplate = ReadOnlyDetailTemplate;
+
+                return;
+            }
+
+            DetailTemplate = EditMode == EditMode.Edit ? EditDetailTemplate : ReadOnlyDetailTemplate;
         }
     }
 }
